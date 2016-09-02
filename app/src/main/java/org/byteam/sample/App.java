a207be7facb004638dc652bc99ac01cee4a9e3f5
chenenyu <echocey@gmail.com>
2016-09-02 19:16:57 +0800

    调整demo

--- app/src/main/java/org/byteam/sample/App.java
+++ app/src/main/java/org/byteam/sample/App.java
@@ -1,12 +1,22 @@
 package org.byteam.sample;
 
+import android.content.Context;
 import android.support.multidex.MultiDexApplication;
+
+import org.byteam.tp.Tp;
 
 /**
  * @Author: chenenyu
  * @Created: 16/8/23 16:36.
  */
 public class App extends MultiDexApplication {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        Tp.install(this);
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
--- app/src/main/java/org/byteam/sample/MainActivity.java
+++ app/src/main/java/org/byteam/sample/MainActivity.java
@@ -2,12 +2,35 @@ package org.byteam.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.byteam.tp.Tp;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mFixBtn;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFixBtn = (Button) findViewById(R.id.btn_fix);
+        mFixBtn.setOnClickListener(this);
+        mTextView = (TextView) findViewById(R.id.text);
+        setTextView();
+    }
+
+    private void setTextView() {
+        mTextView.setText("Fixed!");
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mFixBtn) {
+            Tp.applyPatchFromDevice(this);
+        }
     }
 }
