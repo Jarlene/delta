5b53fa91daee259397c92ef0455e6a2a4cd68568
chenenyu <echocey@gmail.com>
2016-09-22 17:03:07 +0800

    little improvement

--- app/src/main/java/org/byteam/delta/sample/App.java
+++ app/src/main/java/org/byteam/delta/sample/App.java
@@ -3,8 +3,6 @@ package org.byteam.delta.sample;
 import android.content.Context;
 import android.support.multidex.MultiDexApplication;
 
-import org.byteam.delta.Delta;
-
 /**
  * @Author: chenenyu
  * @Created: 16/8/23 16:36.
@@ -14,7 +12,6 @@ public class App extends MultiDexApplication {
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
-        Delta.install(this);
     }
 
     @Override
--- app/src/main/java/org/byteam/delta/sample/MainActivity.java
+++ app/src/main/java/org/byteam/delta/sample/MainActivity.java
@@ -1,12 +1,15 @@
 package org.byteam.delta.sample;
 
 import android.os.Bundle;
+import android.os.Process;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.byteam.delta.Delta;
+import org.byteam.delta.PatchListener;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -26,15 +29,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void setTextView() {
-        mTextView.setText("Not fixed!");
+        mTextView.setText("Not Fixed!");
     }
 
     @Override
     public void onClick(View v) {
         if (v == mFixBtn) {
-            Delta.applyPatchFromDevice(this);
+            Delta.applyPatchForTest(getApplicationContext(), new PatchListener() {
+                @Override
+                public void patchResult(boolean result, String message) {
+                    if (result) {
+                        Toast.makeText(getApplicationContext(), "补丁应用成功,请重启查看效果",
+                                Toast.LENGTH_SHORT).show();
+                        mFixBtn.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                Process.killProcess(Process.myPid());
+                                System.exit(0);
+                            }
+                        }, 2000);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "补丁应用失败:" + message,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         } else if (v == mCleanBtn) {
-            Delta.clean(this);
+            Delta.clear(getApplicationContext());
         }
     }
 }
